1b3397689b529cf85be95155ef66722bbf1ad011
sanjeetchoudhary <sanjeet.choudhary1@gmail.com>
2019-12-10 10:39:25 +0530

    Assignment2 changes

--- src/test/java/test/pivotal/pal/tracker/EnvControllerTest.java
+++ src/test/java/test/pivotal/pal/tracker/EnvControllerTest.java
@@ -1,11 +1,11 @@
 package test.pivotal.pal.tracker;
 
-import org.junit.Test;
 
 import java.util.Map;
 import io.pivotal.pal.tracker.EnvController;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class EnvControllerTest {
     @Test
@@ -19,10 +19,10 @@ public class EnvControllerTest {
 
         Map<String, String> env = controller.getEnv();
 
-        assertThat(env.get("PORT")).isEqualTo("8675");
-        assertThat(env.get("MEMORY_LIMIT")).isEqualTo("12G");
-        assertThat(env.get("CF_INSTANCE_INDEX")).isEqualTo("34");
-        assertThat(env.get("CF_INSTANCE_ADDR")).isEqualTo("123.sesame.street");
+        assertEquals("8675",env.get("PORT"));
+        assertEquals("12G",env.get("MEMORY_LIMIT"));
+        assertEquals("34",env.get("CF_INSTANCE_INDEX"));
+        assertEquals("123.sesame.street",env.get("CF_INSTANCE_ADDR"));
     }
 
 }
--- src/test/java/test/pivotal/pal/tracker/WelcomeControllerTest.java
+++ src/test/java/test/pivotal/pal/tracker/WelcomeControllerTest.java
@@ -1,16 +1,18 @@
 package test.pivotal.pal.tracker;
 
 import io.pivotal.pal.tracker.WelcomeController;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import static org.assertj.core.api.Assertions.assertThat;
+
+
 
 public class WelcomeControllerTest {
 
     @Test
     public void itSaysHello() throws Exception {
         WelcomeController controller = new WelcomeController("A welcome message");
+    assertEquals("A welcome message",controller.sayHello());
 
-        assertThat(controller.sayHello()).isEqualTo("A welcome message");
     }
 }
