5838c86ddb316a77379b46b8e186ad970314ed56
sanjeetchoudhary <sanjeet.choudhary1@gmail.com>
2019-12-13 11:01:28 +0530

    Assignment4 junit compilation ifx

--- src/test/java/test/pivotal/pal/tracker/EnvControllerTest.java
+++ src/test/java/test/pivotal/pal/tracker/EnvControllerTest.java
@@ -1,28 +1,28 @@
 package test.pivotal.pal.tracker;
 
+import org.junit.Test;
 
 import java.util.Map;
 import io.pivotal.pal.tracker.EnvController;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class EnvControllerTest {
     @Test
     public void getEnv() throws Exception {
         EnvController controller = new EnvController(
-            "8675",
-            "12G",
-            "34",
-            "123.sesame.street"
+                "8675",
+                "12G",
+                "34",
+                "123.sesame.street"
         );
 
         Map<String, String> env = controller.getEnv();
 
-        assertEquals("8675",env.get("PORT"));
-        assertEquals("12G",env.get("MEMORY_LIMIT"));
-        assertEquals("34",env.get("CF_INSTANCE_INDEX"));
-        assertEquals("123.sesame.street",env.get("CF_INSTANCE_ADDR"));
+        assertThat(env.get("PORT")).isEqualTo("8675");
+        assertThat(env.get("MEMORY_LIMIT")).isEqualTo("12G");
+        assertThat(env.get("CF_INSTANCE_INDEX")).isEqualTo("34");
+        assertThat(env.get("CF_INSTANCE_ADDR")).isEqualTo("123.sesame.street");
     }
 
 }
